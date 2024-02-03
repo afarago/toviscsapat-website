b728a3d5e907dc0729301f7cc36d6ce9aa9d87d8
Attila Farago <attila.farago.hu@gmail.com>
2024-02-03 23:40:31 +0100

    2023 masterpiece update 1

--- docs/img/years/z_create_thumbnails.cmd
+++ docs/img/years/z_create_thumbnails.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\ImageMagick-7.0.11-Q16-HDRI\magick.exe" mogrify -format jpg -path tn -thumbnail "255x255>" "%1" "tn\%1"
+"e:\wutil\ImageMagick-7.1.1-27-portable-Q16-HDRI-x64\magick.exe" mogrify -format jpg -path tn -thumbnail "255x255>" "%1" "tn\%1"
 move "tn\%~n1.jpg" "tn\%~n1_tn.jpg"
